1889f4c8bd8f23e50feb2c82099b7c45b89ca7ad
Nilisha Nagar <111676856+nilishanagar@users.noreply.github.com>
2025-01-14 10:25:08 +0530

    Update RegistrationForm.js

--- frontend/src/components/Login/RegistrationForm.js
+++ frontend/src/components/Login/RegistrationForm.js
@@ -81,7 +81,7 @@ const RegistrationForm = (props) => {
 
     // If no validation errors, you can proceed with form submission logic
     if (props.action === "create")
-    {Axios.post('http://localhost:4000/eventRoute/create-user', formData)
+    {Axios.post('https://event-management-system-backend-eyjc.onrender.com/eventRoute/create-user', formData)
     .then((res) => {
       if(res.status === 200)
       {
@@ -104,7 +104,7 @@ const RegistrationForm = (props) => {
       console.log("From form page:",userData);
       Axios.all([
         // Updating user records
-        Axios.put("http://localhost:4000/eventRoute/update-user/" + props.id, userData)
+        Axios.put("https://event-management-system-backend-eyjc.onrender.com/eventRoute/update-user/" + props.id, userData)
         .then(response => {
           alert('User updated successfully');
 
@@ -113,7 +113,7 @@ const RegistrationForm = (props) => {
           console.error('Error updating user:', error);
         }),
 
-        Axios.get("http://localhost:4000/eventRoute/event-list")
+        Axios.get("https://event-management-system-backend-eyjc.onrender.com/eventRoute/event-list")
         .then((eventResponse) => {
             if(eventResponse.status === 200){
                 //Finding events where current user is registered
